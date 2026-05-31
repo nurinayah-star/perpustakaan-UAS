477d4ec535dcda0d2e0e7de9ef93916fa649e6af
nayy <nurinayahlutfi@gmail.com>
2026-05-31 20:13:46 +0800

    Update coba.py

--- coba.py
+++ coba.py
@@ -16,7 +16,7 @@ class Buku:
         self.penerbit     = penerbit
         self.tahun_terbit = tahun_terbit
         self.jenis_buku   = jenis_buku
-        self.ketersediaan = ketersedii
+        self.ketersediaan = ketersediaan
         self.left  = None
         self.right = None
 
